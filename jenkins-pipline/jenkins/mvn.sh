732639587ae5e76714884cfea96c5475b4d66518
willzhao <595572829@qq.com>
2025-12-24 11:32:43 +0000

    udpate mvn

--- jenkins-pipline/jenkins/mvn.sh
+++ jenkins-pipline/jenkins/mvn.sh
@@ -2,5 +2,5 @@
 echo "*************************"
 echo "********Build jar********"
 echo "*************************"
-WORKSPACE=/workspaces/jenkins/home/home/workspace/pipeline-docker-maven
+WORKSPACE=/workspaces/jenkins/home/home/workspace/maven-test/jenkins-pipline
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven "$@"
