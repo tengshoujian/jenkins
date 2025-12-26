8c32ab9d0fbf59400f20ede2427d27e0eeaefbac
willzhao <595572829@qq.com>
2025-12-26 08:41:08 +0000

    update file

--- jenkins-pipline/jenkins/mvn.sh
+++ jenkins-pipline/jenkins/mvn.sh
@@ -2,5 +2,5 @@
 echo "*************************"
 echo "********Build jar********"
 echo "*************************"
-WORKSPACE=/workspaces/jenkins/home/home/workspace/maven-test/jenkins-pipline
-docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven "$@"
+WORKSPACE=/workspaces/jenkins/home/home/workspace/maven-test/
+docker run --rm -v $WORKSPACE/jenkins-pipline/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven "$@"
