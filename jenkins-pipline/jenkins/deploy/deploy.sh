51a31d5fcebdc11276723a06ac4e1fc00be66ad6
willzhao <595572829@qq.com>
2025-12-29 13:21:54 +0000

    update

--- jenkins-pipline/jenkins/deploy/deploy.sh
+++ jenkins-pipline/jenkins/deploy/deploy.sh
@@ -5,5 +5,6 @@ echo $TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 scp -i /opt/prod /tmp/.auth prod-user@118.178.140.203:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod-user@118.178.140.203:/tmp/publish
+scp -i /opt/prod ./jenkins-pipline/jenkins/deploy/publish prod-user@118.178.140.203:/tmp/publish
 ssh -i /opt/prod prod-user@118.178.140.203 "/tmp/publish"
+
