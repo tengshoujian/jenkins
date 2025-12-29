c8ff06e166a1ca8bc69feae053d31c4fdabff571
willzhao <595572829@qq.com>
2025-12-29 14:23:19 +0000

    update file

--- jenkins-pipline/jenkins/deploy/deploy.sh
+++ jenkins-pipline/jenkins/deploy/deploy.sh
@@ -7,5 +7,5 @@ echo $PASS >> /tmp/.auth
 
 scp -i /opt/prod /tmp/.auth prod-user@118.178.140.203:/tmp/.auth
 scp -i /opt/prod ./jenkins-pipline/jenkins/deploy/publish prod-user@118.178.140.203:/tmp/publish
-ssh -i /opt/prod prod-user@118.178.140.203 "/tmp/publish"
+ssh -i /opt/prod prod-user@118.178.140.203 "bash /tmp/publish"
 
