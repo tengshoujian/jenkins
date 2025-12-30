2fa6c97bc3801453da6de5277c518964d7d4c47c
willzhao <595572829@qq.com>
2025-12-30 02:22:14 +0000

    udpate

--- jenkins-pipline/jenkins/deploy/deploy.sh
+++ jenkins-pipline/jenkins/deploy/deploy.sh
@@ -5,7 +5,7 @@ echo maven-project > /tmp/.auth
 echo $TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth prod-user@8.219.160.167:/tmp/.auth
-scp -i /opt/prod ./jenkins-pipline/jenkins/deploy/publish prod-user@8.219.160.167:/tmp/publish
-scp -i /opt/prod ./jenkins-pipline/jenkins/deploy/docker-compose prod-user@8.219.160.167:~/maven
-ssh -i /opt/prod prod-user@8.219.160.167 "bash /tmp/publish"
+scp -i /opt/prod /tmp/.auth prod-user@8.219.119.72:/tmp/.auth
+scp -i /opt/prod ./jenkins-pipline/jenkins/deploy/publish prod-user@8.219.119.72:/tmp/publish
+scp -i /opt/prod ./jenkins-pipline/jenkins/deploy/docker-compose prod-user@8.219.119.72:~/maven
+ssh -i /opt/prod prod-user@8.219.119.72 "bash /tmp/publish"
